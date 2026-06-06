e898a1a5df459d7e8f33c8d2897c308bb62708e9
Kajal Gupta <kk2347994@gmail.com>
2026-06-06 16:46:41 +0530

    first commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const app = require("./src/app");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;  // PORT env variable
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
