f58e833ed57c25a1137fe1f0e3c7381af3dc26a6
Kajal Gupta <kk2347994@gmail.com>
2026-06-06 18:16:07 +0530

    first commit

--- src/config/db.js
+++ src/config/db.js
@@ -5,7 +5,11 @@ const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    connectTimeout: 30000,
+    waitForConnections: true,
+    connectionLimit: 10,
 });
 
 module.exports = db;
